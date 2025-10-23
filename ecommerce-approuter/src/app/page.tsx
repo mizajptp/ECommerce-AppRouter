42f5064ffa3ff41c5f013853bcc094eb244a13f3
Mizaj <mizajptp@gmail.com>
2025-10-23 21:46:29 +0530

    changes in the home page to make image section responsive

--- ecommerce-approuter/src/app/page.tsx
+++ ecommerce-approuter/src/app/page.tsx
@@ -10,27 +10,27 @@ export default function Home() {
         <h3>Great Deals - 499 only!!</h3>
         <div className="container">
           <div className="row text-center">
-            <div className="col-6 col-md-3 fs-6">
+            <div className="col-12 col-sm-6 col-lg-3 fs-6 ">
               <Link href="">
-                <Image className="m-3" src="/images/image1.jpg" alt="image" height={200} width={180} />
+                <Image className="my-3" src="/images/image1.jpg" alt="image" height={200} width={200} />
               </Link>
               <p>Women's Heels, Flats...</p>
             </div>
-            <div className="col-6 col-md-3 fs-6">
+            <div className="col-12 col-sm-6 col-lg-3 fs-6 ">
               <Link href="">
-                <Image className="m-3" src="/images/image2.jpg" alt="image" height={200} width={180} />
+                <Image className="my-3" src="/images/image2.jpg" alt="image" height={200} width={200} />
               </Link>
               <p>Men's Sports Shoes...</p>
             </div>
-            <div className="col-6 col-md-3 fs-6">
+            <div className="col-12 col-sm-6 col-lg-3 fs-6 ">
               <Link href="">
-                <Image className="m-3" src="/images/image4.jpg" alt="image" height={200} width={180} />
+                <Image className="my-3" src="/images/image4.jpg" alt="image" height={200} width={200} />
               </Link>
               <p>USPA, Levi's, Peter Engl...</p>
             </div>
-            <div className="col-6 col-md-3 fs-6">
+            <div className="col-12 col-sm-6 col-lg-3 fs-6 ">
               <Link href="">
-                <Image className="m-3" src="/images/image5.jpg" alt="image" height={200} width={180} />
+                <Image className="my-3" src="/images/image5.jpg" alt="image" height={200} width={200} />
               </Link>
               <p>Kids Casual Shoes...</p>
             </div>
